32e707e61b264d9f52ef94f49fd2f9c07fe913c6
Brian Woodward <brian.woodward@gmail.com>
2014-12-11 17:14:24 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-render <https://github.com/assemble/assemble-render>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
